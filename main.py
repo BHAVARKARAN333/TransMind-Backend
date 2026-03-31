4f566f1a0ee7706e2f82cb9e40af1aa1dba5a486
Bhavar Karan <bhavarkaran888@gmail.com>
2026-03-31 17:34:56 +0530

    fix: allow all origins in CORS to support Vercel deployment

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
